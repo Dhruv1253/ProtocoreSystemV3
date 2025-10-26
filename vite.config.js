6f017696b4b1f36247de4531d688dd9f0c5226d5
Dhruv Gaund <dhruv1253@gmail.com>
2025-10-26 22:40:50 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Do NOT set root/base unless you need to.
-})
